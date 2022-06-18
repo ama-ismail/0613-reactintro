bbdb3c449cbd1f9f4b4c21e354262ae04917977d
ama-ismail <106986019+ama-ismail@users.noreply.github.com>
2022-06-18 18:06:36 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div class="container">
+      <img
+        src="https://source.unsplash.com/gYl-UtwNg_I/1500x1500"
+        alt="Hey"
+        value="Hey"
+        style={{ height: "100vh", width: "20%" }}
+      ></img>
+      <div class="centered1">HEY</div>
+      <img
+        src="https://source.unsplash.com/rFKUFzjPYiQ/1500x1500"
+        alt=""
+        style={{ height: "100vh", width: "20%" }}
+      ></img>
+      <div class="centered2">LET'S</div>
+      <img
+        src="https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d"
+        alt=""
+        style={{ height: "100vh", width: "20%" }}
+      ></img>
+      <div class="centered3">GIVE</div>
+      <img
+        src="https://source.unsplash.com/ITjiVXcwVng/1500x1500"
+        alt=""
+        style={{ height: "100vh", width: "20%" }}
+      ></img>
+      <div class="centered4">All</div>
+      <img
+        src="https://source.unsplash.com/3MNzGlQM7qs/1500x1500"
+        alt=""
+        style={{ height: "100vh", width: "20%" }}
+      ></img>
+      <div class="centered5">YOU CAN</div>
+    </div>
+  );
+}
